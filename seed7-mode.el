04c31f37010e0e81afbeb2157451a52b3f15944c
Pierre Rouleau <prouleau001@gmail.com>
2025-07-03 10:42:39 -0400

    removed 'out' from seed7--declaration-intro-keywords
    There's no 'out' parameter in Seed7.
    Added link to documentation.

--- seed7-mode.el
+++ seed7-mode.el
@@ -2,7 +2,7 @@
 
 ;; Created   : Wednesday, March 26 2025.
 ;; Author    : Pierre Rouleau <prouleau001@gmail.com>
-;; Time-stamp: <2025-07-03 10:25:22 EDT, updated by Pierre Rouleau>
+;; Time-stamp: <2025-07-03 10:39:56 EDT, updated by Pierre Rouleau>
 
 ;; This file is not part of GNU Emacs.
 
@@ -443,7 +443,7 @@
 ;;* Version Info
 ;;  ============
 
-(defconst seed7-mode-version-timestamp "2025-07-03T14:25:22+0000 W27-4"
+(defconst seed7-mode-version-timestamp "2025-07-03T14:39:56+0000 W27-4"
   "Version UTC timestamp of the seed7-mode file.
 Automatically updated when saved during development.
 Please do not modify.")
@@ -984,13 +984,14 @@ Has only one capturing group.")
 
 ;;** Seed7 declaration introduction keywords
 ;;   ---------------------------------------
+;;
+;; Ref: Parameters: https://seed7.net/manual/params.htm
 
 (defconst seed7--declaration-intro-keywords
   '("attr"
     "const"
     "in"
     "inout"
-    "out"
     "ref"
     "val"
     "var"))
