4de4eb8deb2c06e08bb2141164fc15e114838ab1
Pierre Rouleau <prouleau001@gmail.com>
2025-07-14 11:49:26 -0400

    Add directory to test basic Emacs functionality and seed7-mode
    Some of seed7-mode code must comply with some basic Emacs functionality.
    
    The documentation of that Emacs basic functionality is not always clear to me,
    so instead of making assumptions on how it is supposed to work I am writing
    tests code and example code to check the behaviour of some of those basic
    Emacs functionalities in various major modes, including emacs-lisp-mode,
    seed7-mode, python-mode and possibly some other major modes.
    This way I can ensure that the seed7 mode code implements what is required and
    I can also potentially spot issues inside the logic of the implementation of
    those base Emacs functionalities if there are any and compare those
    functionalities over several versions of Emacs.

---
+++ tests/emacs-analysis/elisp-functions.el
@@ -0,0 +1,35 @@
+;; Emacs Lisp Example Code Used For Tests
+
+(defun fct-b ()
+  "First test function."
+  (message "fct-b"))
+
+
+(defun fct-b ()
+  "First test function."
+  (message "fct-b"))
+
+
+;; comment before fct-c
+(defun fct-c ()
+  "First test function."
+  (message "fct-c"))
+
+
+;; comment before fct-d
+
+(defun fct-d ()
+  "First test function."
+  (message "fct-d"))
+
+
+;; comment line 1 before (fct-e)
+;; comment line 2 before fct-e
+;; comment line 3 before fct-e
+
+
+(defun fct-e ()
+  "First test function."
+  (message "fct-e"))
+
+;; -----last line (line 35) ---------------------------------------------------
