f0dd259ff760ad16a9180c2db23d719bc550a289
Pierre Rouleau <prouleau001@gmail.com>
2025-07-14 12:40:14 -0400

    Add test code for C and Python

---
+++ tests/emacs-analysis/c-functions.c
@@ -0,0 +1,35 @@
+#include <stdio.h> /* C example code for Tests */
+
+void greet_1() {
+    printf("Bonjour\n");
+}
+
+
+void greet_2() {
+    printf("Buongiorno\n");
+}
+
+
+/* comment before function 3 */
+void greet_3() {
+    printf("Guten tag\n");
+}
+
+
+/* comment before function 4 */
+
+void greet_4() {
+    printf("Buen día\n");
+}
+
+
+/* comment line 1 before function 5 */
+/* comment line 2 before function 5
+ * followed by another one */
+
+
+void greet_5() {
+    printf("Bom dia\n");
+}
+
+/* --------last line (line 35) ---------------------------------------------- */
---
+++ tests/emacs-analysis/python-functions.py
@@ -0,0 +1,35 @@
+# Python Example Code for Tests
+
+def greet_1(text='Bonjour'):
+    """ Print greeting."""
+    print("%s\n" % text)
+
+
+def greet_2(text='Buongiorno'):
+    """ Print greeting."""
+    print("%s\n" % text)
+
+
+# comment before function 3
+def greet_3(text='Guten morgen'):
+    """ Print greeting."""
+    print("%s\n" % text)
+
+
+# comment before function 4
+
+def greet_4(text='Buen día'):
+    """ Print greeting."""
+    print("%s\n" % text)
+
+
+# comment line 1 before function 5
+# comment line 2 before function 5
+# comment line 3 before function 5
+
+
+def greet_5(text='Bom dia'):
+    """ Print greeting."""
+    print("%s\n" % text)
+
+# ----------last line (line 35) ----------------------------------------------
